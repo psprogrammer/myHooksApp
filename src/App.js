cca7c6ec12850ce2c9de7f9c6001a66ad672b09c
Somdatt Bhadvariya <somdattbhadvariya@Somdatts-MacBook-Air.local>
2022-02-04 13:22:43 +0530

    search bar changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Search from "./components/Search";
 
 function App() {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,41 @@
-import React,{useEffct,useState} from 'react';
-import axios from 'axios'
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 
 const Search = () =>{
 const [text,setText]= useState("");
 const [results,setresult]=useState([]);
 
-useEffct(()=>{
-    const data = async =() => {
-        
-        axios.get('')}
+
+useEffect(() => {
+    const search = async () => {
+      const data = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+            action: 'query',
+            list: 'search',
+            format: 'json',
+            origin: '*',
+            srsearch: 'programming'
+        },
+            
+       });
+       console.log("data",data);
+       setresult(data.data.query.search)
+    };
+    search();
 },[text])
 
-
+const renderItems = results.map((result)=>{
+return (
+    <div>
+        <div>
+            {result.title}
+        </div>
+        <div>
+            {result.snippet}
+        </div>
+    </div>
+)
+})
     return(
         <div>
             <div>
@@ -20,6 +44,9 @@ useEffct(()=>{
                 value={text}
                 onChange={e=>setText(e.target.value)}
                 type="text"></input>
+            </div>
+            <div>
+                {renderItems}
             </div>
         </div>
     )
